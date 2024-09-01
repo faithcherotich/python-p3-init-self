33fbf7bb3c40723e1a1ac3ac055ca94adea7a058
faithcherotich <faithcherotich910@gmail.com>
2024-09-01 17:47:29 +0300

    commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+   def __init__(self,name,breed="Mutt"):
+       
+       self.breed = breed
+       self.name = name
+       
+fido = Dog("Fido", "Dalmatian")   
+    
+
+    
